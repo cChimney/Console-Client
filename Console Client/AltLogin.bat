fee64286e971d2b2c1eaf4ce28047cc1b4ec7d6d
cChimney <66905361+cChimney@users.noreply.github.com>
2020-10-11 13:07:31 +0200

    Console Client

---
+++ Console Client/AltLogin.bat
@@ -0,0 +1,15 @@
+start /min MinecraftClient.exe 
+timeout 15
+start /min MinecraftClient.exe 
+timeout 15
+start /min MinecraftClient.exe 
+timeout 15
+start /min MinecraftClient.exe 
+timeout 15
+start /min MinecraftClient.exe 
+timeout 15
+start /min MinecraftClient.exe 
+timeout 15
+
+
+
